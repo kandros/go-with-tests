4c0524dd1a0c9d7703742ef48638d089c7edf077
Jaga Santagostino <jagasantagostino@gmail.com>
2019-04-26 21:56:25 +0200

    add reflection

---
+++ reflection/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walk(val.MapIndex(key).Interface(), fn)
+		}
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		return val.Elem()
+	}
+
+	return val
+}
+
+func main() {
+	fmt.Println("hello")
+}
---
+++ reflection/main_test.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{
+				"Chris", "London",
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Pointer to thing",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
+		{
+			"Arrys",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
+	}
+
+	for _, test := range cases {
+		var got []string
+		walk(test.Input, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, test.ExpectedCalls) {
+			t.Errorf("got %#v, want %#v", got, test.ExpectedCalls)
+		}
+	}
+
+	t.Run("with maps (as order is not preserver)", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+}
+
+func assertContains(t *testing.T, got []string, want string) {
+	contains := false
+	for _, x := range got {
+		if x == want {
+			contains = true
+		}
+	}
+
+	if !contains {
+		t.Errorf("expected %+v to contian '%s' but it didn't", got, want)
+	}
+}
