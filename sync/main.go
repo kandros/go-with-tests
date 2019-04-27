dda40644b264e75c4ac19c4bee06c702d8dd0380
Jaga Santagostino <jagasantagostino@gmail.com>
2019-04-27 22:31:49 +0200

    sync

---
+++ sync/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
+
+func main() {
+	fmt.Println("hello")
+}
